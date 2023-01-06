8002afec573dacd1a7bd188354f970841eed5eb1
Evacd <evangelina.schnaider@gmail.com>
2023-01-06 16:21:11 +0300

    pep8

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -5,7 +5,12 @@ from django.http import HttpResponse
 # Главная страница
 def index(request):
     template = 'posts/index.html'
-    return render(request, template)
+    title = 'Это главная страница проекта Yatube'
+    context = { 'title': title }
+    return render(request, template, context)
 
 def group_posts(request):
-    return HttpResponse('Посты, отфильтрованные по группам')
+    template = 'posts/group_list.html'
+    text = 'Здесь будет информация о группах проекта Yatube'
+    context = { 'text': text }
+    return render(request, template, context)
